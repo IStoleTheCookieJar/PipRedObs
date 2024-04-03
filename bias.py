83e957bffd9db38208e287f2d9a7f99430c41dfe
IStoleTheCookieJar <adams.kalvyn@gmail.com>
2024-04-02 19:20:57 -0600

    First Draft Bias.py

--- bias.py
+++ bias.py
@@ -5,15 +5,35 @@ from astropy.io import fits
 import astropy.units as u
 import pandas as pd
 import glob
+import sys
 import os
-#i have no clue how much of this needs to be imported here, i've never made a pipeline before 
 
-pathname = ""
+if(len(sys.argv)<2):
+    sys.exit("Not enough arguements, try 'py bias.py ~datadirectory~'")
+
+#Mac users should just be able to copy absolute path
+#Windows should change absolute paths to directories from C:\User\...\ to /Users/.../
+#In Kalvyn's case: /Users/Kalvyn/Desktop/Obs2/APO_Data/
+
+pathname = sys.argv[1]
 biasfiles = glob.glob(pathname +"bias*")
+if(len(biasfiles) == 0):
+    sys.exit("No bias files found in chosen directory")
 usefiles = glob.glob(pathname+"*galspec*")
+if(len(usefiles) == 0):
+    sys.exit("No galspec files found in chosen directory")
 
-bias_image_array = np.zeros(len(biasfiles))
+bias0 = fits.open(biasfiles[0])
+size = np.shape(bias0[0].data)
+# print(size)
+bias_image_array = np.zeros((len(biasfiles),size[0],size[1]))
+
 for i in range(len(biasfiles)):
   bias = fits.open(biasfiles[i])
   bias_image = bias[0].data
   bias_image_array[i] = bias_image
+
+bias_image_array = np.mean(bias_image_array,axis=0)
+###Creating Fits file
+hdu = fits.PrimaryHDU(bias_image_array)
+hdu.writeto("FullBias.fits")
