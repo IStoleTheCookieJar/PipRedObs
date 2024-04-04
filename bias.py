9d9447831699f63e9e2fa504738cfc8efd2c3a84
IStoleTheCookieJar <adams.kalvyn@gmail.com>
2024-04-03 19:55:36 -0600

    Second Draft bias.py

--- bias.py
+++ bias.py
@@ -1,12 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
 from astropy.io import fits
-import astropy.units as u
-import pandas as pd
 import glob
 import sys
-import os
 
 if(len(sys.argv)<2):
     sys.exit("Not enough arguements, try 'py bias.py ~datadirectory~'")
@@ -15,6 +10,7 @@ if(len(sys.argv)<2):
 #Windows should change absolute paths to directories from C:\User\...\ to /Users/.../
 #In Kalvyn's case: /Users/Kalvyn/Desktop/Obs2/APO_Data/
 
+#####File finding############
 pathname = sys.argv[1]
 biasfiles = glob.glob(pathname +"bias*")
 if(len(biasfiles) == 0):
@@ -22,18 +18,46 @@ if(len(biasfiles) == 0):
 usefiles = glob.glob(pathname+"*galspec*")
 if(len(usefiles) == 0):
     sys.exit("No galspec files found in chosen directory")
+flatfiles = glob.glob(pathname+"flat*")
+if(len(flatfiles) == 0):
+    sys.exit("No flat files found in chosen direcotry")
 
+####Opening vias files#######
 bias0 = fits.open(biasfiles[0])
 size = np.shape(bias0[0].data)
 # print(size)
 bias_image_array = np.zeros((len(biasfiles),size[0],size[1]))
 
+#######Reading bias files and averaging###########
 for i in range(len(biasfiles)):
   bias = fits.open(biasfiles[i])
   bias_image = bias[0].data
   bias_image_array[i] = bias_image
 
-bias_image_array = np.mean(bias_image_array,axis=0)
-###Creating Fits file
-hdu = fits.PrimaryHDU(bias_image_array)
-hdu.writeto("FullBias.fits")
+bias_image_final = np.mean(bias_image_array,axis=0)
+
+#######Subtracting from all use files and creating new ones##########
+for i in range(len(usefiles)):
+    if(usefiles[i][-6:] != "B.fits"):
+        print(usefiles[i])
+        current = fits.open(usefiles[i])
+        current_image = current[0].data
+        reduced = current_image-bias_image_final
+        temp = fits.PrimaryHDU(reduced)
+        temp.header = current[0].header
+        temp.writeto(usefiles[i][:-5]+"B.fits",overwrite=True)
+
+########Subtracting from all flat files and creating new ones#########
+for i in range(len(flatfiles)):
+    if(flatfiles[i][-6:] != "B.fits"):
+        print(flatfiles[i])
+        current = fits.open(flatfiles[i])
+        current_image = current[0].data
+        reduced = current_image-bias_image_final
+        temp = fits.PrimaryHDU(reduced)
+        temp.header = current[0].header
+        temp.writeto(flatfiles[i][:-5]+"B.fits",overwrite=True)
+
+###Creating Fits file##############
+hdu = fits.PrimaryHDU(bias_image_final)
+hdu.writeto("FullBias.fits",overwrite=True)
