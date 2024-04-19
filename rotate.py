3e0a5f2fd5fe61597a5f717c992e2476ccc8f051
IStoleTheCookieJar <adams.kalvyn@gmail.com>
2024-04-18 22:54:20 -0600

    z tentative

--- rotate.py
+++ rotate.py
@@ -13,7 +13,9 @@ pathname = sys.argv[1]
 usefiles = glob.glob(pathname+"*galspec*.F.fits")
 if(len(usefiles) == 0):
     sys.exit("No galspec files found in chosen directory")
-
+use2files = glob.glob(pathname+"*galspec*.D.fits")
+if(len(use2files) == 0):
+    sys.exit("No galspec files found in chosen directory")
 stdfiles = glob.glob(pathname+"std*.F.fits")
 if(len(stdfiles) == 0):
     sys.exit("No std files found in chosen direcotry")
@@ -34,6 +36,17 @@ for i in range(len(usefiles)):
 
         current.writeto(usefiles[i].replace(".fits", ".R.fits"), overwrite=True)
         print("Saved {0}".format(usefiles[i].replace(".fits", ".R.fits")))
+
+for i in range(len(use2files)):
+    if(use2files[i][-6:] == "D.fits"):
+        current = fits.open(use2files[i])
+
+        current[0].data = rotate(current[0].data, rotate_val, reshape=False)
+
+        current[0].data = current[0].data[ycrop[0]:ycrop[1], xcrop[0]:xcrop[1]]
+
+        current.writeto(use2files[i].replace(".fits", ".R.fits"), overwrite=True)
+        print("Saved {0}".format(use2files[i].replace(".fits", ".R.fits")))
 
 
 ######Rotating all std files and creating new ones##########
